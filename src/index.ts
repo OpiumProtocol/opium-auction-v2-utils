a8612adac4f10c6c4c4d37c10c01530b4248d61c
dmytrobaida <dmytrobaida@outlook.com>
2023-01-12 11:50:44 +0200

    exports fn from lib

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { AuctionPricingDirection, AuctionPricingFunction, buildAuctionOrder, encodeOrder } from './build'
 import { EthersSignerConnector } from './connectors'
+import { GetRemainingPrice } from './price'
 
 export {
   AuctionPricingDirection, AuctionPricingFunction, buildAuctionOrder, encodeOrder,
-  EthersSignerConnector
+  EthersSignerConnector,
+  GetRemainingPrice,
 }
