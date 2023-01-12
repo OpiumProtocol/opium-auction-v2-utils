d7586defe24ac12a5f59e75aa8851634590a77cb
dmytrobaida <dmytrobaida@outlook.com>
2023-01-12 11:49:10 +0200

    adds price calculation fn

---
+++ src/price.ts
@@ -0,0 +1,28 @@
+import { LimitOrder, LimitOrderBuilder, LimitOrderProtocolFacade } from "@1inch/limit-order-protocol";
+
+export async function GetRemainingPrice(options: {
+    limitOrderProtocolFacade: LimitOrderProtocolFacade,
+    limitOrderBuilder: LimitOrderBuilder,
+    limitOrder: LimitOrder,
+}) {
+    const { limitOrderProtocolFacade, limitOrderBuilder, limitOrder } = options;
+
+    const limitOrderTypedData = limitOrderBuilder.buildLimitOrderTypedData(limitOrder)
+    const orderHash = limitOrderBuilder.buildLimitOrderHash(limitOrderTypedData)
+
+    try {
+        const remaining = await limitOrderProtocolFacade.remaining(
+            orderHash
+        );
+
+        return remaining.toString();
+    } catch (error: any) {
+        const errorMessage = typeof error === 'string' ? error : error.message;
+
+        if (errorMessage.includes('LOP: Unknown order')) {
+            return limitOrder.makingAmount;
+        }
+
+        throw error;
+    }
+}
